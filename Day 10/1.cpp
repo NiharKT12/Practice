944be339e65547c2f8ab269d0994905943087d3e
NiharKT12 <niharkt336@gmail.com>
2026-06-09 21:54:48 +0530

    Day 10

---
+++ Day 10/1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode{
+    int val;
+    TreeNode *r;
+    TreeNode *l;
+    TreeNode(int val){
+        this->val = val;
+        r = nullptr;
+        l = nullptr;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
